1494ab316d542bc80b2d747dc9488e687923dc20
YunEunJeong <araruu2536@gmail.com>
2021-12-03 16:06:33 +0900

    Update lobby.py
    Second

--- lobby.py
+++ lobby.py
@@ -1,15 +1,51 @@
 from tictactoe import*
 
 def lobby():
-    print("1. 3*3")
-    print("2. 5*5")
-    lobby1 = input()
-    if (lobby1 == "1"):
+    print("틱택토 게임")
+    print("1. 게임 모드 선택")
+    print("2. 게임 설명")
+    print("3. 게임 종료")
+    menu = input("선택지를 입력하세요")
+    if (menu == "1"):
+        print("1테스트")
+        game_lobby()
+    elif (menu == "2"):
+        print("2테스트")
+        game_expalnation()
+    elif (menu == "3"):
+        print("3테스트")
+        quit()
+    else:
+        print("올바르지 않은 입력입니다.")
+        lobby()
+        
+def game_lobby():
+    print("1. 3X3 틱택토 (1인)")
+    print("2. 3X3 틱택토 (2인)")
+    print("3. 5X5 틱택토 (1인)")
+    print("4. 5X5 틱택토 (2인)")
+    menu = input("선택지를 입력하세요")
+    if (menu == "1"):
+        print("1테스트")
+        game_start()
+    elif (menu == "2"):
+        print("2테스트")
+        game_start()
+    elif (menu == "3"):
+        print("3테스트")
+        game_start()
+    elif (menu == "4"):
+        print("4테스트")
         game_start()
     else:
-        print("나중에 오세요")
-            
+        print("올바르지 않은 입력입니다.")
+        game_lobby()
+        
+def game_expalnation():
+    print("틱택토 게임은 어쩌고 입니다.")
+    print("돌아가려면 엔터키를 입력하세요.")
+    input()
+    lobby()
+        
 lobby()
-
-
 
